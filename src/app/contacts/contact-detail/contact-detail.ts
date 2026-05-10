b164b893ef2aadc552f984b12739dd3a6ff7aedb
eseegm97 <eseegm97@gmail.com>
2026-05-09 23:50:05 -0500

    event_emitters

--- src/app/contacts/contact-detail/contact-detail.ts
+++ src/app/contacts/contact-detail/contact-detail.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Contact } from '../contact.model';
 
 @Component({
@@ -8,5 +8,6 @@ import { Contact } from '../contact.model';
   styleUrl: './contact-detail.css',
 })
 export class ContactDetail {
+  @Input()
   contact: Contact = new Contact('', '', '', '', '', null);
 }
