f958257a828e1efb7734a6c9e5c5c4a7b51edd94
eseegm97 <eseegm97@gmail.com>
2026-05-10 00:24:42 -0500

    create_messages_component

---
+++ src/app/messages/message-edit/message-edit.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-message-edit',
+  imports: [],
+  templateUrl: './message-edit.html',
+  styleUrl: './message-edit.css',
+})
+export class MessageEdit {}
---
+++ src/app/messages/message-list/message-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-message-list',
+  imports: [],
+  templateUrl: './message-list.html',
+  styleUrl: './message-list.css',
+})
+export class MessageList {}
