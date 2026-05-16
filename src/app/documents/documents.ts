72f4d67b914e761e815f6e45083b52a4cc86dad3
eseegm97 <eseegm97@gmail.com>
2026-05-15 22:04:50 -0500

    implementing_documents_component_overall

--- src/app/documents/documents.ts
+++ src/app/documents/documents.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 import { DocumentList } from "./document-list/document-list";
 import { DocumentDetail } from "./document-detail/document-detail";
+import { Document } from './document.model';
 
 @Component({
   selector: 'cms-documents',
-  imports: [DocumentList, DocumentDetail],
+  imports: [NgIf, DocumentList, DocumentDetail],
   templateUrl: './documents.html',
   styleUrl: './documents.css',
 })
-export class Documents {}
+export class Documents {
+  selectedDocument!: Document;
+}
