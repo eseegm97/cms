6e0ce8328bced9dfd11f53a57301ae419c53401f
eseegm97 <eseegm97@gmail.com>
2026-05-23 23:24:32 -0500

    implement_the_message_service

--- src/app/messages/message-list/message-list.ts
+++ src/app/messages/message-list/message-list.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MessageEdit } from '../message-edit/message-edit';
 import { Message } from '../message.model';
 import { MessageItemComponent } from '../message-item/message-item';
+import { MessageService } from '../message';
 
 @Component({
   selector: 'cms-message-list',
@@ -10,14 +11,14 @@ import { MessageItemComponent } from '../message-item/message-item';
   templateUrl: './message-list.html',
   styleUrl: './message-list.css',
 })
-export class MessageList {
-  messages: Message[] = [
-    new Message('1', 'Grades', 'The grades for this assignment have been posted', 'Bro. Jackson'),
-    new Message('2', 'Assignment 3', 'When is assignment 3 due', 'Steve Johnson'),
-    new Message('3', 'Re: Assignment 3', 'Assignment 3 is due on Saturday at 11:30 PM', 'Bro. Jackson'),
-    new Message('3', 'Help with assignment 3', 'Can I meet with you sometime. I need help with assignment 3', 'Mark Smith'),
-    new Message('3', 'Re: Help with assignment 3', 'I can meet with you today at 4:00 PM in my office.', 'Bro. Jackson')
-  ];
+export class MessageList implements OnInit {
+  messages: Message[] = [];
+
+  constructor(private messageService: MessageService) {}
+
+  ngOnInit(): void {
+    this.messages = this.messageService.getMessages();
+  }
 
   onAddMessage(message: Message): void {
     this.messages.push(message);
