e3dea1eb1b2b655a5797a257acfe5a79ee99e357
Ethan Seegmiller <eseegm97@gmail.com>
2026-06-08 13:35:40 -0500

    create_form_to_add_and_edit_contacts

--- src/app/contacts/contact-edit/contact-edit.ts
+++ src/app/contacts/contact-edit/contact-edit.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { ContactItem } from '../contact-item/contact-item';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'cms-contact-edit',
-  imports: [],
+  imports: [NgFor, NgIf, ContactItem],
   templateUrl: './contact-edit.html',
   styleUrl: './contact-edit.css',
 })
-export class ContactEdit {}
+export class ContactEdit {
+  groupContacts: Contact[] = [];
+
+  onRemoveItem(index: number) {
+    this.groupContacts.splice(index, 1);
+  }
+
+  onCancel() {}
+}
