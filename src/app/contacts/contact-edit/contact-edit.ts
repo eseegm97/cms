57ff4d00d277937cb4de9144e4b69303a9458945
Ethan Seegmiller <eseegm97@gmail.com>
2026-06-08 13:53:08 -0500

    validate_contact_edit_form_fields

--- src/app/contacts/contact-edit/contact-edit.ts
+++ src/app/contacts/contact-edit/contact-edit.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { NgFor, NgIf } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
 import { ContactItem } from '../contact-item/contact-item';
 import { Contact } from '../contact.model';
 
 @Component({
   selector: 'cms-contact-edit',
-  imports: [NgFor, NgIf, ContactItem],
+  imports: [NgFor, NgIf, FormsModule, ContactItem],
   templateUrl: './contact-edit.html',
   styleUrl: './contact-edit.css',
 })
 export class ContactEdit {
+  contact: Contact | null = null;
   groupContacts: Contact[] = [];
+
+  onSubmit(form: NgForm) {}
 
   onRemoveItem(index: number) {
     this.groupContacts.splice(index, 1);
