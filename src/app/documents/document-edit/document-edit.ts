b52e58f38c0a76e7ec172f455bffe76fe366caec
Ethan Seegmiller <eseegm97@gmail.com>
2026-06-08 13:20:03 -0500

    implement_field_validation_for_document_edit

--- src/app/documents/document-edit/document-edit.ts
+++ src/app/documents/document-edit/document-edit.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Document } from '../document.model';
+import { Document as DocumentService } from '../document';
 
 @Component({
   selector: 'cms-document-edit',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './document-edit.html',
   styleUrl: './document-edit.css',
 })
-export class DocumentEdit {
-  onCancel() {}
+export class DocumentEdit implements OnInit {
+  originalDocument!: Document;
+  document: Document = new Document('', '', '', '', null);
+  editMode: boolean = false;
+
+  constructor(private documentService: DocumentService) {}
+
+  ngOnInit(): void {}
+
+  onSubmit(form: NgForm): void {
+    const value = form.value;
+    const newDocument = new Document('', value.name, value.description, value.url, null);
+    if (this.editMode) {
+      this.documentService.updateDocument(this.originalDocument, newDocument);
+    } else {
+      this.documentService.addDocument(newDocument);
+    }
+    this.onCancel();
+  }
+
+  onCancel(): void {
+    this.editMode = false;
+  }
 }
